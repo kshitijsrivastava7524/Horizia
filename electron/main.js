4fa1f8fa41f5f5ffb325dba9c851b7f5b83fbba7
Ayu-shhhh <ayushsrivastava09032004@gmail.com>
2026-04-21 16:46:52 +0530

    front end wip

--- electron/main.js
+++ electron/main.js
@@ -1,13 +1,33 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-function createWindow() {
-  const win = new BrowserWindow({
-    width: 1200,
-    height: 800
-  });
+//autoreload
+// const fs = require('fs');
+const chokidar = require('chokidar');
 
-  win.loadFile(path.join(__dirname, '../frontend/index.html'));
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        show: false,
+        autoHideMenuBar: true
+    });
+
+    win.loadFile(path.join(__dirname, '../frontend/index-testing.html'));
+    win.setResizable(true);
+
+    // Watch frontend folder
+    chokidar.watch(path.join(__dirname, '../frontend')).on('change', () => {
+        console.log("Reloading...");
+        win.webContents.reload();
+    });
+
+
+    win.maximize();
+    win.once('ready-to-show', () => {
+        win.show()
+    });
 }
 
 app.whenReady().then(createWindow);
